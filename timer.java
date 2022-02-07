9602d276a0c74e14ef42feda0e137716a972905d
kovacs-balazs <56174644+kovacs-balazs@users.noreply.github.com>
2022-02-07 18:06:28 +0100

    Update timer.java

--- timer.java
+++ timer.java
@@ -1,1 +1,17 @@
+public static boolean isRunning = false;
 
+public void timer() {
+	if(isRunning) {
+		return;
+	}
+	isRunning = true;
+	Timer timer = new Timer();
+	TimerTask task = new TimerTask() {
+		@Override
+			public void run() {
+			isRunning = false;
+		// cancel(); // Cancel the timer if you wanna make an delayed function
+		}
+	};
+	timer.schedule(task, 1000L, 1L);
+}
