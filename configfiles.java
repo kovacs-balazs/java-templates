1cd0c244c98619e2d796e1a38e1d163cebbede8d
kovacs-balazs <56174644+kovacs-balazs@users.noreply.github.com>
2022-02-07 18:18:33 +0100

    Update configfiles.java

--- configfiles.java
+++ configfiles.java
@@ -1,33 +1,34 @@
-private static File languagecfg;
-    private static FileConfiguration languagefile;
-    public static Main m = Main.getPlugin(Main.class);
+private static File namecfg;
+private static FileConfiguration namefile;
 
-    public static void setup() {
-        languagecfg = new File(m.getDataFolder(), "language_en.yml");
-        if(!languagecfg.exists()) {
-            try {
-                languagecfg.createNewFile();
-            } catch (IOException e) {
-                // owww
-            }
-        }
-        languagefile = YamlConfiguration.loadConfiguration(languagecfg);
-    }
+public static Main m = Main.getPlugin(Main.class);
 
-    public static FileConfiguration getLanguage() {
-        return languagefile;
-    }
-
-    public static void saveLanguage() {
-        File file = new File(m.getDataFolder(), "language_en.yml");
-        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+public static void setup() {
+    namecfg = new File(m.getDataFolder(), "name.yml"); //
+    if(!namecfg.exists()) {
         try {
-            languagefile.save(languagecfg);
+            namecfg.createNewFile();
         } catch (IOException e) {
-            System.out.println("Can't save language file");
+            // owww
         }
     }
+    namefile = YamlConfiguration.loadConfiguration(namecfg);
+}
 
-    public static void reloadLanguage() {
-        languagefile = YamlConfiguration.loadConfiguration(languagecfg);
+public static FileConfiguration getName() {
+    return namefile;
+}
+
+public static void saveName() {
+    File file = new File(m.getDataFolder(), "name.yml");
+    YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+    try {
+        namefile.save(namecfg);
+    } catch (IOException e) {
+        System.out.println("Can't save language file");
     }
+}
+
+public static void reloadName() {
+    namefile = YamlConfiguration.loadConfiguration(namecfg);
+}
