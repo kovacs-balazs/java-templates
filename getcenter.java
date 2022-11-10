80bf55d0fb668b9209c9ec8584f9bb9221c34316
Balázs Kovács <56174644+kovacs-balazs@users.noreply.github.com>
2022-11-10 16:01:53 +0100

    Create getcenter.java

---
+++ getcenter.java
@@ -0,0 +1,12 @@
+public static Location getCenter(Location loc) {
+    return new Location(loc.getWorld(),
+            getRelativeCoord(loc.getBlockX()),
+            getRelativeCoord(loc.getBlockY()),
+            getRelativeCoord(loc.getBlockZ()));
+}
+
+private static double getRelativeCoord(int i) {
+    double d = i;
+    d = d < 0 ? d - .5 : d + .5;
+    return d;
+}
